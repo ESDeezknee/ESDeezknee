206eb0b9b2115886104914e335d44d35e4189ad1
vanleexn <vanessa.lee.2021@scis.smu.edu.sg>
2023-03-12 00:49:13 +0800

    handleGroup complex service

---
+++ handleGroup/handleGroup.py
@@ -0,0 +1,63 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+import os, sys
+
+import requests
+from invokes import invoke_http
+
+app = Flask(__name__)
+CORS(app)
+
+group_URL = "http://127.0.0.1:6103/grouping"
+
+@app.route("/handleGroup", methods=["POST"])
+def create_group():
+    # Simple check of input format and data of the request are JSON
+    if request.is_json:
+        try:
+            group = request.get_json()
+            print("\nReceived a create group request in JSON:", group)
+
+            # do the actual work
+            # 1. Send order info {cart items}
+            result = processCreateGroup(group)
+            return jsonify(result), result["code"]
+
+        except Exception as e:
+            # Unexpected error in code
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            ex_str = str(e) + " at " + str(exc_type) + ": " + fname + ": line " + str(exc_tb.tb_lineno)
+            print(ex_str)
+
+            return jsonify({
+                "code": 500,
+                "message": "handleGroup.py internal error: " + ex_str
+            }), 500
+
+    # if reached here, not a JSON request.
+    return jsonify({
+        "code": 400,
+        "message": "Invalid JSON input: " + str(request.get_data())
+    }), 400
+
+def processCreateGroup(group):
+    print('\n-----Invoking create microservice-----')
+    createGroup_result = invoke_http(group_URL, method='POST', json=group)
+    print('createGroup_result:', createGroup_result)
+
+    code = createGroup_result["code"]
+    if code not in range(200,300):
+        return {
+            "code": 500,
+            "data": {"createGroup_result": createGroup_result},
+            "message": "New group creation failed."
+        }
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=6104, debug=True)
