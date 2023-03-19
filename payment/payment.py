f1b0c9d17e9c2188f7ac982f8b3a8572bc09d573
Law Zer Kye, Keith <keith.law.2021@scis.smu.edu.sg>
2023-03-19 21:13:27 +0800

    edits

--- payment/payment.py
+++ payment/payment.py
@@ -4,11 +4,11 @@ from flask_cors import CORS
 from os import environ
 import stripe
 
-stripe.api_key = environ['STRIPE_API_KEY']
+stripe.api_key = environ.get('STRIPE_API_KEY')
 
 from datetime import datetime
 
-app = Flask(__name__, static_url_path="", static_folder="paymenttest")
+app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('dbURL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'pool_recycle': 299}
@@ -46,14 +46,10 @@ class Payment(db.Model):
 with app.app_context():
     db.create_all()
 
-#Our domain url
-YOUR_DOMAIN = "http://127.0.0.1:6203"
-
 session_id = ""
 
 @app.route('/create-checkout-session', methods=['POST'])
 def create_checkout_session():
-    print("oof u suck")
     global session_id
 
     try:
@@ -65,8 +61,9 @@ def create_checkout_session():
                 }
             ],
             mode = "payment",
-            success_url = YOUR_DOMAIN + "/retrieve-payment-data",
-            cancel_url = YOUR_DOMAIN + "/cancel.html"
+            #success_url and cancel_url leads to the next page depending on payment status (both are built-in to the API)
+            success_url = "http://127.0.0.1:6203/retrieve-payment-data",
+            cancel_url = ""
         )
         session_id = checkout_session.id
     except Exception as e:
@@ -74,8 +71,8 @@ def create_checkout_session():
 
     return redirect(checkout_session.url, code=303)
 
-# able to retrieve paid/unpaid
-@app.route('/retrieve-payment-data', methods=['GET'])
+# if payment is successful, it will push the relevant data to the db
+@app.route('/retrieve-payment-data', methods=['POST'])
 def retrieve_payment_data():
 
     global session_id
@@ -109,68 +106,6 @@ def get_all():
         }
     ), 404
 
-@app.get("/payment/<int:payment_id>")
-def get_by_id(payment_id):
-    payment = Payment.query.filter_by(payment_id=payment_id).first()
-    if payment:
-        return jsonify(
-            {
-                "code": 200,
-                "data": payment.json()
-            }
-        )
-    return jsonify(
-        {
-            "code": 404,
-            "message": "Payment not found."
-        }
-    ), 404
-
-@app.post("/order")
-def create_order():
-    if request.json is None:
-        raise Exception("No data received.")
-    try:
-        data = request.get_json()
-        new_payment = Payment(**data)
-        db.session.add(new_payment)
-        db.session.commit()
-        db.session.refresh(new_payment)
-    except:
-        return jsonify(
-            {
-                "code": 500,
-                "message": "An error occurred creating the payment."
-            }
-        ), 500
-    
-    return jsonify(
-        {
-            "code": 201,
-            "data": new_payment.json()
-        }
-    ), 201
-
-@app.delete("/payment/<int:payment_id>")
-def delete_payment(payment_id):
-    payment = Payment.query.filter_by(payment_id=payment_id).first()
-    if payment:
-        db.session.delete(payment)
-        db.session.commit()
-        return jsonify(
-            {
-                "code": 200,
-                "data": {
-                    "payment_id": payment.payment_id
-                }
-            }
-        )
-    return jsonify(
-        {
-            "code": 404,
-            "message": "Order not found."
-        }
-    ), 404
 
 
 if __name__ == '__main__':
