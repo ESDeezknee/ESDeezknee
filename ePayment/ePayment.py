bfed9ed0acd1528f0217338246fc3a8454e4f530
Law Zer Kye, Keith <keith.law.2021@scis.smu.edu.sg>
2023-04-02 19:51:50 +0800

    rename payment, stripe update (redirect + update db)

--- ePayment/ePayment.py
+++ ePayment/ePayment.py
@@ -5,6 +5,7 @@ from os import environ
 from datetime import datetime
 import asyncio
 
+from invokes import invoke_http
 import stripe
 
 app = Flask(__name__)
@@ -21,8 +22,8 @@ stripe.api_key = environ.get('STRIPE_API_KEY')
 
 order_URL = environ.get('orderURL') or "http://localhost:6201/order/"
 
-class Payment(db.Model):
-    __tablename__ = 'payment'
+class ePayment(db.Model):
+    __tablename__ = 'epayment'
 
     session_id = db.Column(db.String(128), primary_key=True)
     checkout_url = db.Column(db.String(512), nullable=False)
@@ -52,7 +53,7 @@ with app.app_context():
 
 @app.route('/')
 def hello():
-    return 'Hello from the payment Microservice!'
+    return 'Hello from the ePayment Microservice!'
 
 # pls send a POST request to this endpoint to trigger it instantly
 @app.route('/create-checkout-session', methods=['POST'])
@@ -71,8 +72,9 @@ def create_checkout_session():
             ],
             mode = "payment",
             #success_url and cancel_url leads to the next page depending on payment status (both are built-in to the API)
-            success_url = url_for('check_payment_status', session_id=session_id, _external=True),
+
             # cancel_url should bring it back to the main page but cancels payment
+            success_url = url_for('check_payment_status', _external=True),
             cancel_url = order_URL
         )
         session_id = checkout_session.id
@@ -82,14 +84,16 @@ def create_checkout_session():
     except Exception as e:
         return str(e)
     # account_id is hard-coded for now
-    payment = Payment(session_id=session_id, checkout_url = checkout, account_id=1, status=payment_status, price=8, paymentDate=datetime.now())
+    payment = ePayment(session_id=session_id, checkout_url = checkout, account_id=1, status=payment_status, price=8, paymentDate=datetime.now())
     db.session.add(payment)
     db.session.commit()
     return redirect(checkout_session.url, code=303)
 
-@app.route('/check-payment-status/<session_id>', methods=['GET'])
-async def check_payment_status(session_id):
+@app.route('/check-payment-status', methods=['GET'])
+async def check_payment_status(session_id=None):
     try:
+        payment = ePayment.query.filter_by(status='unpaid').first()
+        session_id = payment.session_id
         session = stripe.checkout.Session.retrieve(session_id)
         payment_status = session.payment_status
     except stripe.error.InvalidRequestError:
@@ -99,23 +103,37 @@ async def check_payment_status(session_id):
 
     if payment_status == 'paid':
         # Payment has been successfully made
-        payment = Payment.query.filter_by(session_id=session_id).first()
+        payment = ePayment.query.filter_by(session_id=session_id).first()
         if payment:
             payment.status = 'paid'
-            db.commit.session()
-        return redirect(url_for(order_URL)) # add the route to generate ticket
+            db.session.commit()
+            payment_json = {
+                "account_id": payment.account_id,
+                "session_id": payment.session_id,
+                "payment_method": "external"
+            }
+        create_ticket = invoke_http(
+            order_URL + str(payment.account_id) + "/paying", method='POST', json=payment_json
+        )
+    if create_ticket["code"] in range(500, 600):
+        return jsonify(
+            {
+                "code": 500,
+                "message": "Oops, something went wrong! Order"
+            }
+        ), 500
     elif payment_status == 'unpaid':
         # Payment has not yet been made
         await asyncio.sleep(30)
-        return await check_payment_status(session_id)
+        return await check_payment_status()
     else:
         # Payment has failed or has been refunded
         return "Payment failed or refunded"
 
 
-@app.get("/payment")
+@app.get("/ePayment")
 def get_all():
-    paymentList = Payment.query.all()
+    paymentList = ePayment.query.all()
     if len(paymentList):
         return jsonify(
             {
